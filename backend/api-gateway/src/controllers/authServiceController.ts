9e8624edefe598f6c8994a8c553696b02aa08b54
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-03-30 01:59:25 +0530

    routes modified and removed redundancy in authServiceController in API-Gateway

--- backend/api-gateway/src/controllers/authServiceController.ts
+++ backend/api-gateway/src/controllers/authServiceController.ts
@@ -1,34 +1,47 @@
+import {Request,Response} from "express";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL as string;
 
-export async function signInRequestToAuthService(message: any) {
+export async function signInRequestToAuthService(req: Request, res:Response): Promise<void> {
     try {
-        const response = await axios.post(`${AUTH_SERVICE_URL}/auth/sign-in`, message);
-        return response.data;
+        const {email, password} = req.body;
+        const response = await axios.post(`${AUTH_SERVICE_URL}/auth/sign-in`,{email, password});
+        if (response.data.success) {
+            res.status(200).json(response);
+        } else {
+            res.status(500).json({ success: false, error: "Failed to send message." });
+        }
     } catch (error) {
         console.error("[API Gateway] Failed to reach Auth Service  for sign-in:", error);
-        return { success: false, error: "Auth Service unavailable" };
     }
 }
 
-export async function signUpRequestToAuthService(message: any) {
+export async function signUpRequestToAuthService(req:Request, res:Response): Promise<void> {
     try {
-        const response = await axios.post(`${AUTH_SERVICE_URL}/auth/sign-up`, message);
-        return response.data;
+        const {name, email, password} = req.body;
+        const response = await axios.post(`${AUTH_SERVICE_URL}/auth/sign-up`,{name, email, password});
+    if (response.data.success) {
+        res.status(200).json(response);
+    } else {
+        res.status(500).json({ success: false, error: "Failed to send message." });
+    }
     } catch (error) {
         console.error("[API Gateway] Failed to reach Auth Service for sign-up:", error);
-        return { success: false, error: "Auth Service unavailable" };
     }
 }
 
-export async function otpVerificationRequestToAuthService(message: any) {
+export async function otpVerificationRequestToAuthService(req:Request, res:Response):Promise<void> {
     try {
-        const response = await axios.post(`${AUTH_SERVICE_URL}/otp/verify`, message);
-        return response.data;
+        const {otpToken, otp} = req.body;
+        const response = await axios.post(`${AUTH_SERVICE_URL}/otp/verify`, {otpToken, otp});
+        if (response.data.success) {
+    res.status(200).json(response);
+} else {
+    res.status(500).json({ success: false, error: "Failed to send message." });
+}
     } catch (error) {
         console.error("[API Gateway] Failed to reach Auth Service for otp verification:", error);
-        return { success: false, error: "Auth Service unavailable" };
     }
 }
--- backend/api-gateway/src/routes/authRoutes.ts
+++ backend/api-gateway/src/routes/authRoutes.ts
@@ -4,60 +4,11 @@ import {
     signUpRequestToAuthService
 } from "../controllers/authServiceController";
 import router from "./chatRoutes";
-import {authenticateToken} from "../middleware/authMiddleware";
 
-router.post("/sign-in", async (req, res) => {
-    try{
-        const {email, password} = req.body;
+router.post("/sign-in",signInRequestToAuthService);
 
-        // Forward the request to the Chat Service
-        const response = await signInRequestToAuthService({email, password});
+router.post("/sign-up",signUpRequestToAuthService);
 
-        if (response.success) {
-            res.status(200).json(response);
-        } else {
-            res.status(500).json({ success: false, error: "Failed to send message." });
-        }
-    } catch (error) {
-        console.error("[API Gateway] Error:", error);
-        res.status(500).json({ success: false, error: "Internal Server Error" });
-    }
-});
-
-router.post("/sign-up", async (req, res) => {
-    try{
-        const {name, email, password} = req.body;
-
-        // Forward the request to the Chat Service
-        const response = await signUpRequestToAuthService({name, email, password});
-
-        if (response.success) {
-            res.status(200).json(response);
-        } else {
-            res.status(500).json({ success: false, error: "Failed to send message." });
-        }
-    } catch (error) {
-        console.error("[API Gateway] Error:", error);
-        res.status(500).json({ success: false, error: "Internal Server Error" });
-    }
-});
-
-router.post("/verify",async (req, res) => {
-    try{
-        const {otpToken, otp} = req.body;
-
-        // Forward the request to the Chat Service
-        const response = await otpVerificationRequestToAuthService({otpToken, otp});
-
-        if (response.success) {
-            res.status(200).json(response);
-        } else {
-            res.status(500).json({ success: false, error: "Failed to send message." });
-        }
-    } catch (error) {
-        console.error("[API Gateway] Error:", error);
-        res.status(500).json({ success: false, error: "Internal Server Error" });
-    }
-});
+router.post("/verify",otpVerificationRequestToAuthService);
 
 export default router;
